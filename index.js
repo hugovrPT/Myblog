9d4131473fa55f014c16e199085cbacdbb9aed68
unknown <hugoreis230803@gmail.com>
2021-05-20 09:47:44 +0100

    updated

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const { ExpressOIDC } = require('@okta/oidc-middleware');
+const Sequelize = require('sequelize');
+const epilogue = require('epilogue'), ForbiddenError = epilogue.Errors.ForbiddenError;
 const app = express();
 const port = 3000;
 
@@ -32,11 +34,65 @@ const oidc = new ExpressOIDC({
 
 // ExpressOIDC will attach handlers for the /login and /authorization-code/callback routes
 app.use(oidc.router);
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome!!</h1>');
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/home.html'));
 });
 
-app.listen(port, () => console.log(`My Blog App listening on port ${port}!`))
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/admin.html'));
+});
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/home');
+});
+
+app.get('/', (req, res) => {
+  res.redirect('/home');
+});
+
+const database = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db.sqlite',
+    operatorsAliases: false,
+});
+
+const Post = database.define('posts', {
+    title: Sequelize.STRING,
+    content: Sequelize.TEXT,
+});
+
+epilogue.initialize({ app, sequelize: database });
+
+const PostResource = epilogue.resource({
+    model: Post,
+    endpoints: ['/posts', '/posts/:id'],
+});
+
+PostResource.all.auth(function (req, res, context) {
+    return new Promise(function (resolve, reject) {
+        //if (!req.isAuthenticated()) {
+            //res.status(401).send({ message: "Unauthorized" });
+           // resolve(context.stop);
+        //} else {
+            resolve(context.continue);
+       // }
+    })
+});
+
+database.sync().then(() => {
+    oidc.on('ready', () => {
+        app.listen(port, () => console.log(`My Blog App listening on port ${port}!`))
+    });
+});
+
+oidc.on('error', err => {
+    // An error occurred while setting up OIDC
+    console.log("oidc error: ", err);
+});
